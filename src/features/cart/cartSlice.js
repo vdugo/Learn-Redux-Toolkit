261e97437542f3491b3f8e88a7c22405d051d930
vdugo <v3rdugo@yahoo.com>
2022-08-06 01:39:22 -0700

    removeItem reducer

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -14,10 +14,14 @@ const cartSlice = createSlice({
     reducers: {
         clearCart: (state) => {
             state.cartItems = []
+        },
+        removeItem: (state, action) => {
+            const itemId = action.payload
+            state.cartItems = state.cartItems.filter((item) => item.id !== itemId)
         }
     }
 })
 
-export const { clearCart } = cartSlice.actions
+export const { clearCart, removeItem } = cartSlice.actions
 
 export default cartSlice.reducer
