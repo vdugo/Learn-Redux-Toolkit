2d0f420fd97efe0e3a154f917bae3647a630bd83
vdugo <v3rdugo@yahoo.com>
2022-08-06 01:46:16 -0700

    increaseItem and decreaseItem reducers

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -18,10 +18,18 @@ const cartSlice = createSlice({
         removeItem: (state, action) => {
             const itemId = action.payload
             state.cartItems = state.cartItems.filter((item) => item.id !== itemId)
+        },
+        increaseItem: (state, {payload}) => {
+            const cartItem = state.cartItems.find((item) => item.id === payload.id)
+            cartItem.amount = cartItem.amount + 1
+        },
+        decreaseItem: (state, {payload}) => {
+            const cartItem = state.cartItems.find((item) => item.id === payload.id)
+            cartItem.amount = cartItem.amount - 1
         }
     }
 })
 
-export const { clearCart, removeItem } = cartSlice.actions
+export const { clearCart, removeItem, increaseItem, decreaseItem } = cartSlice.actions
 
 export default cartSlice.reducer
