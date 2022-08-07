c4e03a3080ff01ffbb8b42da6bdf5658e6d94d79
vdugo <v3rdugo@yahoo.com>
2022-08-07 02:40:52 -0700

    create Modal component, reducer, implement functionality

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -1,6 +1,7 @@
 import CartItem from './CartItem'
 import { useSelector, useDispatch } from 'react-redux'
 import { clearCart } from '../features/cart/cartSlice'
+import { openModal } from '../features/modal/modalSlice'
 
 import React from 'react'
 
@@ -36,7 +37,7 @@ const CartContainer = () => {
                         total <span>${total.toFixed(2)}</span>
                     </h4>
                 </div>
-                <button className='btn clear-btn' onClick={() => dispatch(clearCart())}>
+                <button className='btn clear-btn' onClick={() => dispatch(openModal())}>
                     clear cart
                 </button>
             </footer>
